f45f0b46bdb31ccb667933ff8507786c4bd6381b
camorrow <camorrow@asu.edu>
2017-01-14 23:46:10 -0700

    Small logic update

--- HackAZProject/GreenFoot/app/src/main/java/sodanoobs/greenfoot/TravelTypeHomeScreen.java
+++ HackAZProject/GreenFoot/app/src/main/java/sodanoobs/greenfoot/TravelTypeHomeScreen.java
@@ -1,5 +1,6 @@
 package sodanoobs.greenfoot;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,34 @@ public class TravelTypeHomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_travel_type_home_screen);
     }
 
-    public void toPointsScreen (View v){
-        startActivity(new Intent(TravelTypeHomeScreen.this, PointsScreen.class));
+
+
+    public void walkToPointsScreen (View v){
+        Intent intent = new Intent(TravelTypeHomeScreen.this, PointsScreen.class).putExtra("travelConstant", 0);
+        startActivity(intent);
+    }
+
+    public void busToPointsScreen(View v){
+        Intent intent = new Intent(TravelTypeHomeScreen.this, PointsScreen.class).putExtra("travelConstant", 40);
+        startActivity(intent);
+    }
+
+    public void motorcycleToPointsScreen(View v){
+        Intent intent = new Intent(TravelTypeHomeScreen.this, PointsScreen.class).putExtra("travelConstant", 145);
+        startActivity(intent);
     }
 
     public void toCarScreen (View v){
         startActivity(new Intent(TravelTypeHomeScreen.this, CarScreen.class));
     }
 }
+
+/*
+car- small: 175
+    medium: 215
+    large: 300
+    electric: 80
+motorcycle: 145
+walk: 0
+bus: 40
+ */
